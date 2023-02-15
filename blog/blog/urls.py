24aab9fe39909357ea3511cc49efe82381e9b21f
home pc <you@example.com>
2023-02-16 01:08:05 +0600

    add header navbar

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -5,9 +5,9 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('a/', include('blog_app.urls')),
-    path('a/', include('django.contrib.auth.urls')),
+    #path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('blog_app.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
 
 handler404 = page404
