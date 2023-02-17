7e003ac778c774d8ebd2cd7c472f41f33ae4c27b
home pc <you@example.com>
2023-02-18 03:38:11 +0600

    add custom sign up template

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 from blog_app.views import page404
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('blog_app.urls')),
     path('', include('django.contrib.auth.urls')),
 ]
--- blog/blog_app/forms.py
+++ blog/blog_app/forms.py
@@ -4,7 +4,45 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=255,
+        label="username",
+        widget=forms.TextInput(attrs={
+            'class': 'w-100 form-control',
+            'placeholder': 'username',
+            'id': 'a'
+        }
+        )
+    )
+    email = forms.CharField(
+        max_length=255,
+        label="email",
+        widget=forms.TextInput(attrs={
+            'class': 'w-100 form-control',
+            'placeholder': 'email',
+            'id': 'b'
+        }
+        )
+    )
+    password1 = forms.CharField(
+        label="password1",
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-100 form-control',
+            'placeholder': 'password1',
+            'id': 'b'
+        }
+        )
+    )
+    password2 = forms.CharField(
+        label="password2",
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-100 form-control',
+            'placeholder': 'password2',
+            'id': 'c'
 
+        }
+        )
+    )
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
