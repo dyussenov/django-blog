3e74f1417b164783d0c88783b9597cdd1759cc6f
home pc <you@example.com>
2023-02-19 22:05:32 +0600

    add categories page

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import SignUpView
-
+from django.views.generic import TemplateView
 from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('about/', about, name='about'), 
+    path('about/', about, name='about'),
+    path('categories/', TemplateView.as_view(template_name='categories_list.html'), name='categories_list'),
     path('categories/<str:category>/', categories),
     path('contact/', contact),
     path('archive/<int:year>/', archive)
