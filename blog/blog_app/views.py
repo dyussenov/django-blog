3343ddf68ce5d43e3861af0edf4405e2db473db3
home pc <you@example.com>
2023-03-07 03:38:29 +0600

    add answers in question

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.utils.text import slugify
+from django.views.generic.edit import FormMixin
 
 from .forms import *
 from .models import *
@@ -38,38 +39,40 @@ class QuestionsListView(ListView):
         return questions
 
 
-    '''def get_context_data(self, **kwargs):
-        context = super(QuestionsListView, self).get_context_data(**kwargs)
-        context['filter'] = self.request.GET.get('filter', '')
-        context['orderby'] = self.request.GET.get('orderby', '')
-        return context'''
-
-
-class QuestionView(DetailView):
+class QuestionView(FormMixin, DetailView):
     model = Question
     template_name = 'question.html'
     slug_url_kwarg = 'question_slug'
     context_object_name = 'question'
+    form_class = AddAnswerForm
 
     def get_context_data(self, *args, **kwargs):
         context = super(QuestionView, self).get_context_data(*args, **kwargs)
-        context['answer_form'] = AddAnswerForm
+        context['answers'] = Answer.objects.filter(question_id=self.object)
         return context
+
+    def get_success_url(self):
+        return reverse_lazy('show_question', kwargs={'question_slug': self.object.slug})
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.question = self.object
+        print(form.data)
+        form.save()
+        return super(QuestionView, self).form_valid(form)
 
 
 def index(request):
     return render(request, "home.html")
 
-
-'''def category_questions(request, category_slug):
-    category = get_object_or_404(Category, slug=category_slug)
-    questions = Question.objects.filter(category=category)
-    context = {
-        'cat_selected': category,
-        'questions': questions
-    }
-    return render(request, "questions.html", context)
-'''
 
 def page404(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
